e9a857d264eb32d9a4de75a80d0866178e79242d
Teodor Garzdin <teodorgarzedin@gmail.com>
2017-02-18 15:10:27 +0100

    Add a better logout animation, initial view controller slides from top down.

--- MonPay/AccountSettingsViewController.swift
+++ MonPay/AccountSettingsViewController.swift
@@ -19,10 +19,10 @@ class AccountSettingsViewController: UIViewController {
         Keychain.sharedInstance.delete("refresh_token")
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let initialViewController = storyboard.instantiateViewController(withIdentifier: "InitialViewController")
-        initialViewController.view.alpha = 0
+        initialViewController.view.frame = CGRect(x: initialViewController.view.frame.origin.x, y: -(initialViewController.view.frame.size.width * 2), width: initialViewController.view.frame.size.width, height: initialViewController.view.frame.size.height)
         UIView.transition(with: UIApplication.shared.keyWindow!, duration: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
             UIApplication.shared.keyWindow?.rootViewController = initialViewController
-            initialViewController.view.alpha = 1
+            initialViewController.view.frame = CGRect(x: initialViewController.view.frame.origin.x, y: 0, width: initialViewController.view.frame.size.width, height: initialViewController.view.frame.size.height)
         }, completion: nil)
     }
 }
